00a55594022ea8f8222f09f8bf99e8b51e04be21
aLIEzsss4 <vuetomato@gmail.com>
2022-11-09 23:32:57 +0800

    deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+require("dotenv").config();
+
+const accounts = [process.env.PRIVATEKEY!];
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_URL,
+      accounts: accounts,
+    },
+  },
+
+  namedAccounts: {
+    deployer: 0,
+  },
 };
 
 export default config;
